7a69c15b8bf0d3601235864aead5e684d558e53a
James Bunker <97250900+JamesBunker@users.noreply.github.com>
2023-12-01 16:49:51 -0500

    Update options.py

--- youtube_dl/options.py
+++ youtube_dl/options.py
@@ -131,7 +131,7 @@ def parseOpts(overrideArguments=None):
     # Newly added option for transcript generation
     general.add_option(
         '--generate-transcript',
-        dest='transcription', default=False,
+        action='store_true', dest='transcription', default=False,
         help='Write transcript file')
 
     general.add_option(
